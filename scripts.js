bad12bd8f4fd1d593c7d57da6a20d9a45138983c
gremaxusPonderPad <thomas.and.mercer@gmail.com>
2024-07-08 19:58:23 -0400

    Currently experimenting with function logic, this is all untested still.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 function buttonOperation() {
+    buttonNumbers();
     const operation = document.getElementsByClassName("btn-operation");
+
+    switch (operation) {
+        case '+':
+
+    }
+
 }
 
 function buttonNumbers() {
+    const numbers = [];
 
+    for (const nums of numbers) {
+        numbers.push(nums);
+    }
+    
+    const numNumbers = nums;
+
+    return numbers, numNumbers;
 }
