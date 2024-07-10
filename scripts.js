e3b8eb5a5ca2dcdd77698dfb1f39bdf9ab19682b
Gremaxus <thomas.and.mercer@gmail.com>
2024-07-10 12:32:54 -0400

    Added in some notes at the bottom of scripts.js referencing a previous project that may help provide me a solution with creating handlers with dynamic capabilities.

--- scripts.js
+++ scripts.js
@@ -50,8 +50,58 @@ function buttonNumbers() {
 function getOperand() {
   const addition = document.getElementById("operation-addition").value;
   const subtraction = document.getElementById("operation-subtraction").value;
-  const multiplication = document.getElementById(
-    "operation-multiplication"
-  ).value;
+  const multiplication = document.getElementById("operation-multiplication").value;
   const division = document.getElementById("operation-division").value;
 }
+
+function checkOperation() {
+    let operation;
+    
+    switch (operation) {
+        case "+":
+          console.log("+");
+          return document.getElementById("operation-addition").value;       
+    
+        case "-":
+          console.log("-");
+          return document.getElementById("operation-subtraction").value;
+             
+        case "*":
+          console.log("*");
+          return document.getElementById("operation-multiplication").value;         
+    
+        case "/":
+          console.log("/");
+          return document.getElementById("operation-division").value;          
+    
+        case "=":
+          console.log("=");
+          return document.getElementById("equals").value;
+    
+        //default:
+         //return alert(
+            //"The operations and/or numbers you have entered is invalid."
+          //);
+      }
+}
+
+function addition() {
+    const addition = document.getElementById("operation-addition").value;
+}
+
+function subtraction() {
+    const subtraction = document.getElementById("operation-subtraction").value;
+}
+
+function multiplication() {
+    const multiplication = document.getElementById("operation-multiplication").value;
+}
+
+function division() {
+    const division = document.getElementById("operation-division").value;
+}
+
+/*
+const ids = ["uppercase", "lowercase", "numbers", "special"];
+const properties = {};
+*/
